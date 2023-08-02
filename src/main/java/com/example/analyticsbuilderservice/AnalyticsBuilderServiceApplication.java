22a0a225a2ae658e4829d4785e2198e6235a9b5a
Костя <k.shylau1991@gmail.com>
2023-08-02 18:39:47 +0300

    feat: create project

--- src/main/java/com/example/analyticsbuilderservice/AnalyticsBuilderServiceApplication.java
+++ src/main/java/com/example/analyticsbuilderservice/AnalyticsBuilderServiceApplication.java
@@ -8,6 +8,7 @@ public class AnalyticsBuilderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsBuilderServiceApplication.class, args);
+
     }
 
 }
